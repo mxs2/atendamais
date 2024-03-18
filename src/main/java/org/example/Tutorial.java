9b139f16651def4b1ab026b3bee137b0dc53a23f
mxs2 <mxs2@cin.ufpe.br>
2024-03-18 10:54:52 -0300

    init commit

---
+++ src/main/java/org/example/Tutorial.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Tutorial {
+    public static void main(String [] args) {
+        // Tipos primitivos: inteiros - reais - lógicos - textuais
+        // Strings guardam referências a objetos, diferente do char, que é um tipo primitivo (valor)
+        int valor01;
+        float valor02;
+        boolean valor03;
+        char letra;
+        valor01 = 12;
+        valor02 = 12.85f;
+        valor03 = true;
+        letra = 'A';
+        System.out.println(valor01);
+        System.out.println(valor02);
+        System.out.println(valor03);
+        System.out.println(letra);
+        System.out.println("Parabéns por ter chegado até aqui!");
+    }
+}
